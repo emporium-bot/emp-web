415591ce5c93d33de3bc831710d9eb157ea09d61
Felipe Mendes <zo.fmendes@gmail.com>
2022-07-22 20:05:06 -0300

    feat: add log utils

---
+++ src/utils/log.ts
@@ -0,0 +1,13 @@
+export abstract class AppLog {
+	static readonly IDENTIFIER = '[emp-web]';
+
+	static error(...args: any[]): void {
+		// eslint-disable-next-line no-console
+		console.error(this.IDENTIFIER, ...args);
+	}
+
+	static warn(...args: any[]): void {
+		// eslint-disable-next-line no-console
+		console.warn(this.IDENTIFIER, ...args);
+	}
+}
