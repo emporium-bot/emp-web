04afca191d51a08d9806a39b1d654dd37c77f149
Felipe Mendes <zo.fmendes@gmail.com>
2022-07-22 20:34:37 -0300

    feat: add plug button base functions

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import ReactDOM from 'react-dom';
+import { Provider as ReduxProvider } from 'react-redux';
 import { App } from '@/app';
 import { ThemeProvider } from './theme';
+import { store } from './store';
 
 ReactDOM.render(
-	<ThemeProvider>
-		<App />
-	</ThemeProvider>,
+	<ReduxProvider store={store}>
+		<ThemeProvider>
+			<App />
+		</ThemeProvider>
+	</ReduxProvider>,
 	document.getElementById('app-root')
 );
